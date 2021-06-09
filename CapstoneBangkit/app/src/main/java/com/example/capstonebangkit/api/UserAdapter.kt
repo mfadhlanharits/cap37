e647dcbdcffd45aee9c214397aff40ae9fdda5dd
Lunnardo <lunnardolukias@gmail.com>
2021-06-09 19:14:31 +0700

    Smartket - Capstone 0037 - Bangkit

--- CapstoneBangkit/app/src/main/java/com/example/capstonebangkit/api/UserAdapter.kt
+++ CapstoneBangkit/app/src/main/java/com/example/capstonebangkit/api/UserAdapter.kt
@@ -4,10 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Filter
-import android.widget.Filterable
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
